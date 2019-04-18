a9632a133e0b8c8b27354e1b2d035b1df44fe16c
Peter <peter3923@gmail.com>
2019-04-18 20:38:31 +0000

    update prepare node script

--- prepare_node.sh
+++ prepare_node.sh
@@ -8,9 +8,10 @@ rm -rf terra-chain-spec
 git clone https://github.com/ContractLand/terra-chain-spec.git
 cd terra-chain-spec
 git checkout $NETWORK
+cd ..
 
 # Populate config with env variables
-sed -i "s/NODE_NAME/$NODE_NAME/g" monitor/app.json
-sed -i "s/DASHBOARD_IP/$DASHBOARD_IP/g" monitor/app.json
-sed -i "s/DASHBOARD_PORT/$DASHBOARD_PORT/g" monitor/app.json
-sed -i "s/DASHBOARD_SECRET/$DASHBOARD_SECRET/g" monitor/app.json
+sed -i "s/NODE_NAME/$NODE_NAME/g" ./monitor/app.json
+sed -i "s/DASHBOARD_IP/$DASHBOARD_IP/g" ./monitor/app.json
+sed -i "s/DASHBOARD_PORT/$DASHBOARD_PORT/g" ./monitor/app.json
+sed -i "s/DASHBOARD_SECRET/$DASHBOARD_SECRET/g" ./monitor/app.json
